21a545edbd28716b659c5ff523928eba60435131
András Ács <andras.acs@dealogic.com>
2019-08-19 07:51:18 +0200

    Supress accessability warning with making the home emoji ARIA compatible.

--- src/components/presenters/BackToHome.js
+++ src/components/presenters/BackToHome.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
 class BackToHome extends React.Component {
-    render() {
-        return <div className="home-icon">
-            <a href="/" className="home-icon__hyperlink">&#x1F3E0;</a>
-        </div>;
-    }
+  render() {
+    return (
+      <div className="home-icon">
+        <a href="/" className="home-icon__hyperlink">
+          <span
+            className="home-iconm__emoji"
+            role="img"
+            aria-label="Go back to home."
+          >
+            &#x1F3E0;
+          </span>
+        </a>
+      </div>
+    );
+  }
 }
 
 export default BackToHome;
