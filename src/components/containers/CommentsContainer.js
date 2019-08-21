e4d6a30ec8329fcac429db595046bfdd8c943dd6
András Ács <mr.acs.andras@gmail.com>
2019-08-21 21:47:04 +0200

    Rendering comments in from the recursive data source.

--- src/components/containers/CommentsContainer.js
+++ src/components/containers/CommentsContainer.js
@@ -12,34 +12,73 @@ class CommentsContainer extends React.Component {
       this.context.url + `item/${parentId}.json`
     );
     const parentEntiy = await parentEntityResponse.json();
-    parentEntiy.kids.forEach(async commentId => {
+    for (let commentId of parentEntiy.kids) {
       const commentResponse = await fetch(
         this.context.url + `item/${commentId}.json`
       );
       const comment = await commentResponse.json();
+      await this.getChildren(comment, 0);
       comments.push(comment);
       this.setState({ comments });
-    });
+    }
+  }
+
+  async getChildren(comment, level) {
+    comment.level = level;
+    if (comment.kids) {
+      for (let kidId of comment.kids) {
+        const kidResponse = await fetch(
+          this.context.url + `item/${kidId}.json`
+        );
+        const kid = await kidResponse.json();
+        if (!comment.hasOwnProperty("children")) {
+          comment.children = [];
+        }
+        comment.children.push(kid);
+        await this.getChildren(kid, ++level);
+      }
+    }
+  }
+
+  getMargin(level) {
+    return { marginLeft: `${level * 10}px` };
+  }
+
+  renderComment(comment) {
+    let kids = null;
+    if (comment.children) {
+      kids = comment.children.map(kid => this.renderComment(kid));
+    }
+    if (comment.text) {
+      return (
+        <div
+          key={comment.id}
+          className="comment"
+          style={this.getMargin(comment.level)}
+        >
+          <div className="comment__title">
+            ▲ {comment.by} - {moment.unix(comment.time).fromNow()}
+          </div>
+          <div
+            className="comment__content"
+            dangerouslySetInnerHTML={{
+              __html: comment.text
+                .replace(/(<? *script)/gi, "illegalscript")
+                .replace(/<a/gi, '<a class="comment__hyperlink" ')
+            }}
+          />
+          {kids}
+        </div>
+      );
+    }
+    return <></>;
   }
 
   render() {
     return (
       <div className="comments-container">
-        {this.state.comments.map(comment => (
-          <div key={comment.index} className="comment">
-            <div className="comment__title">
-              ▲ {comment.by} - {moment.unix(comment.time).fromNow()}
-            </div>
-            <div
-              className="comment__content"
-              dangerouslySetInnerHTML={{
-                __html: comment.text
-                  .replace(/(<? *script)/gi, "illegalscript")
-                  .replace(/<a/gi, '<a class="comment__hyperlink" ')
-              }}
-            />
-          </div>
-        ))}
+        {this.state.comments
+          .map(comment => this.renderComment(comment))}
       </div>
     );
   }
